971114350d5f4af83cdcc60ebb1acbaf862ab927
stanzzza <va11no@yandex.ru>
2022-03-04 13:55:02 +0300

    init

--- src/start.ts
+++ src/start.ts
@@ -5,14 +5,6 @@ import config from 'dotenv/config';
 export function startApp() {
   // getTokens();
 
-  /**
-   * @description Write your channels in format:
-   * { name: Notify server name, serverId, channelId }
-   */
-  // const projects = [
-  //   {name: 'My server', serverId: '935140299582365756', channelId: '935140299582365760'}
-  // ]
-
   const projects = config2.invitePicker.projects
 
   projects.forEach(project => {
